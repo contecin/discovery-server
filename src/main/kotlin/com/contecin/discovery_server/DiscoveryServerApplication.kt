fa9b5cd2ce16cf4ba1a3e563b758447ca9664ca7
diego_protec <diegosfprotec@gmail.com>
2023-03-23 18:59:19 -0300

    primeira versão do serviço retornando 200

--- src/main/kotlin/com/contecin/discovery_server/DiscoveryServerApplication.kt
+++ src/main/kotlin/com/contecin/discovery_server/DiscoveryServerApplication.kt
@@ -2,7 +2,9 @@ package com.contecin.discovery_server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 class DiscoveryServerApplication
 
